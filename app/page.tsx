33c17c74e76b7c40d83584af9abee094a449b343
Wilfred Osozi <osoziw@gmail.com>
2025-11-29 13:08:38 +0300

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -43,19 +43,24 @@ const handleNumberClick = (num: string) => setGuess((prev) => prev + num);
 
         if (num < secretNumber) {
             throw new ValueTooLowError();
+          
         }
         else if (num > secretNumber) {
             throw new ValueTooHighError();
+            
         }
         else {
             setMessage(`You got it right! The number was ${num}. Total attempts: ${attempts + 1}`);
-        }
+            }
     } catch (error) {
         if (error instanceof ValueTooHighError || error instanceof ValueTooLowError) {
             setMessage(`${error.message}`);
         } else {
             setMessage(`Unexpected error: ${error}`);
         }
+    }finally{
+        setAttempts((prev) => prev + 1);
+        setGuess("");
     }
 }
 
@@ -63,7 +68,9 @@ const numbers = Array.from({ length: 10 }, (_, i) => i.toString());
 
 return(
   <div className="min-h-screen flex flex-col items-center justify-center bg-yellow-100 p-4 border-8 border-purple-400 rounded-3xl shadow-2xl">
-    <h1 className="text-3xl sm:text-4xl font-extrabold text-purple-700 mb-6 text-center">🎯 Number Game</h1>
+    <h1 className="text-3xl sm:text-4xl font-extrabold text-purple-700 mb-6 text-center">🎯 Guess Game</h1>
+
+    <p className="w-full text-center text-gray-700 mb-4 px-3 py-2 rounded-lg bg-yellow-50 shadow-sm">🎯 Guess the <span className="text-red-600 font-semibold">secret number</span> between 0 and 100. Click the numbers below to enter your guess, then press "Submit Guess". Try to get it right in as few attempts as possible</p>
     <div className="max-w-md flex flex-col items-center justify-center">
       <p className="w-full text-lg sm:text-xl font-semibold mb-2 text-center text-green-600"
       > Current guess: <span className="font-mono text-gray-800">{guess || "-"}</span>
@@ -83,10 +90,7 @@ return(
       ))}
 
       <div className="col-span-5 grid grid-cols-2 gap-2 sm:gap-4">
-        <button onClick={handleClear}
-      className="col-span-3 bg-red-400 text-white font-bold py-4 sm:py-5 rounded-lg shadow-lg hover:bg-red-500 transition transform hover:scale-105 text-lg sm:text-xl"
-      >Clear</button>
-
+      
       <button onClick={handleSubmit}
       className="col-span-3 bg-green-400 text-white font-bold py-4 sm:py-5 rounded-lg shadow-lg hover:bg-green-500 transition transform hover:scale-105 text-lg sm:text-xl"
       >Submit Guess
